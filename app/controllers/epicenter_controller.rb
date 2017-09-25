0da495a1342e41387f76fe051f6239dd54c2bc51
carterfiber <gerald@carterfiber.com>
2017-09-25 11:21:48 -0400

    added follow and unfollow methods

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -1,13 +1,35 @@
 class EpicenterController < ApplicationController
+
+	before_action :authenticate_user!
+
   def feed
+  	@following_tweets = Array.new
+
+  	Tweet.all.each do |t|
+  		if current_user.following.include?(t.user_id) || current_user.id == t.user_id
+  			@following_tweets.push(t)
+  		end 
+  	end
   end
 
   def show_user
+  	@user = User.find(params[:id])
   end
 
   def now_following
+  	# We are adding the user.id of the user you want to 
+  # follow to your following array.
+  # and we want to make sure it's saved in there as an integer
+  current_user.following.push(params[:id].to_i)
+  current_user.save
+
+  redirect_to show_user_path(id: params[:id])
   end
 
   def unfollow
+  	current_user.following.delete(params[:id].to_i)
+  	current_user.save
+
+  	redirect_to show_user_path(id: params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   get 'epicenter/feed'
 
-  get 'epicenter/show_user'
+  get  'show_user' => 'epicenter#show_user'
 
-  get 'epicenter/now_following'
+  get 'now_following' => 'epicenter#now_following'
 
-  get 'epicenter/unfollow'
+  get  'unfollow' => 'epicenter#unfollow'
+
+  root 'epicenter#feed'
 
   resources :tweets
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'tweets#index'
+  
 end
